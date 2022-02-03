dc6e45047c03bb06256ff33c954fd107cdc00073
SapumalPriyashantha <sapumalpriyashantha2000@gmail.com>
2022-02-04 02:42:13 +0530

    Add data to tables using jquery

--- myprojects/SPA/assests/js/script.js
+++ myprojects/SPA/assests/js/script.js
@@ -16,3 +16,17 @@ document.getElementById("order").addEventListener("click", function () {
     document.getElementById("second_row").style.display = "none";
     document.getElementById("third_row").style.display = "block";
 })
+
+$("#btn_custoomer_save").click(function () {
+
+    let custId = $("#exampleInputCustomerId_1").val();
+    let custName = $("#exampleInputCustomerName_1").val();
+    let custAdress = $("#exampleInputCustomerAddress_1").val();
+
+    let row = `<tr><td>${custId}</td><td>${custName}</td><td>${custAdress}</td></tr>`;
+    $("#customer_Table").append(row);
+
+    $("#customer_Table>tr").click(function(){
+        console.log(this);
+    });
+});
