b3ea216f0fdb3e5f53d23e4912608c5360238ecb
SapumalPriyashantha <sapumalpriyashantha2000@gmail.com>
2022-02-10 04:50:05 +0530

    some features updated in customer

--- myprojects/SPA/assests/js/script.js
+++ myprojects/SPA/assests/js/script.js
@@ -1,32 +1,96 @@
-// document.getElementsByTagName("h3")[0].style.color='red';
-document.getElementById("customer").addEventListener("click", function () {
-    document.getElementById("second_row").style.display = "none";
-    document.getElementById("third_row").style.display = "none";
-    document.getElementById("first_row").style.display = "block";
+$("#customer").click(function () {
+    $("#second_row").css("display", "none");
+    $("#third_row").css("display", "none");
+    $("#first_row").css("display", "block");
+})
+$("#item").click(function () {
+    $("#first_row").css("display", "none");
+    $("#third_row").css("display", "none");
+    $("#second_row").css("display", "block");
+})
+$("#order").click(function () {
+    $("#first_row").css("display", "none");
+    $("#second_row").css("display", "none");
+    $("#third_row").css("display", "block");
 })
 
-document.getElementById("item").addEventListener("click", function () {
-    document.getElementById("first_row").style.display = "none";
-    document.getElementById("third_row").style.display = "none";
-    document.getElementById("second_row").style.display = "block";
-})
+$("#exampleInputCustomerId_1").keydown(function (event) {
+    if (event.key == "Shift") {
+        $("#exampleInputCustomerName_1").focus();
+    }
+});
 
-document.getElementById("order").addEventListener("click", function () {
-    document.getElementById("first_row").style.display = "none";
-    document.getElementById("second_row").style.display = "none";
-    document.getElementById("third_row").style.display = "block";
-})
+$("#exampleInputCustomerName_1").keydown(function (event) {
+    if (event.key == "Shift") {
+        $("#exampleInputCustomerAddress_1").focus();
+    }
+});
+
+$("#exampleInputCustomerAddress_1").keydown(function (event) {
+    if (event.key == "Shift") {
+
+        $("#exampleInputCustomerId_1").focus();
+
+        let custId_2 = $("#exampleInputCustomerId_1").val(); //get first input field value
+        let custName_2 = $("#exampleInputCustomerName_1").val(); //get second input field value
+        let custAdress_2 = $("#exampleInputCustomerAddress_1").val(); //get third input field value
+
+        let row_1 = "<tr><td>" + custId_2 + "</td><td>" + custName_2 + "</td><td>" + custAdress_2 + "</td></tr>";
+
+        //set the row
+        $("#customer_Table").append(row_1);
+
+        //clear the previous text in input filed
+        $("#exampleInputCustomerId_1").val(" ");
+        $("#exampleInputCustomerName_1").val(" ");
+        $("#exampleInputCustomerAddress_1").val(" ");
+
+        $("#customer_Table>tr").click(function () {
+
+            let custId_1 = $(this).children(":eq(0)").text(); // select first td and get text
+            let custName_1 = $(this).children(":eq(1)").text(); // select second td and get text
+            let custAdress_1 = $(this).children(":eq(2)").text(); // select third td and get text
+
+            // set values for the input fields
+            $("#exampleInputCustomerId_1").val(custId_1);
+            $("#exampleInputCustomerName_1").val(custName_1);
+            $("#exampleInputCustomerAddress_1").val(custAdress_1);
+        });
+    }
+});
 
 $("#btn_custoomer_save").click(function () {
+    //Disable previously tr binded function
+    $("#customer_Table>tr").off("click");
 
-    let custId = $("#exampleInputCustomerId_1").val();
-    let custName = $("#exampleInputCustomerName_1").val();
-    let custAdress = $("#exampleInputCustomerAddress_1").val();
+    let custId = $("#exampleInputCustomerId_1").val(); //get first input field value
+    let custName = $("#exampleInputCustomerName_1").val(); //get second input field value
+    let custAdress = $("#exampleInputCustomerAddress_1").val(); //get third input field value
 
-    let row = `<tr><td>${custId}</td><td>${custName}</td><td>${custAdress}</td></tr>`;
+    let row = "<tr><td>" + custId + "</td><td>" + custName + "</td><td>" + custAdress + "</td></tr>";
+
+    //set the row
     $("#customer_Table").append(row);
 
-    $("#customer_Table>tr").click(function(){
-        console.log(this);
+    //focus first input field
+    $("#exampleInputCustomerId_1").focus();
+
+    //clear the previous text in input filed
+    $("#exampleInputCustomerId_1").val(" ");
+    $("#exampleInputCustomerName_1").val(" ");
+    $("#exampleInputCustomerAddress_1").val(" ");
+
+
+    $("#customer_Table>tr").click(function () {
+
+        let custId_1 = $(this).children(":eq(0)").text(); // select first td and get text
+        let custName_1 = $(this).children(":eq(1)").text(); // select second td and get text
+        let custAdress_1 = $(this).children(":eq(2)").text(); // select third td and get text
+
+        // set values for the input fields
+        $("#exampleInputCustomerId_1").val(custId_1);
+        $("#exampleInputCustomerName_1").val(custName_1);
+        $("#exampleInputCustomerAddress_1").val(custAdress_1);
     });
 });
+
